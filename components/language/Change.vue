02ac3e5d4796b7bd18b8d0b3df4da36eb771c2b8
TanaratSudjai <tanarat.dev18@gmail.com>
2025-07-04 00:36:41 +0700

    deployed

--- components/language/Change.vue
+++ components/language/Change.vue
@@ -5,7 +5,7 @@
       :key="item.code"
       @click="setLocale(item.code)"
       :class="[
-        'px-4 py-2 rounded transition hover:cursor-pointer',
+        'px-2 py-2 text-xs rounded transition hover:cursor-pointer',
         locale === item.code
           ? 'bg-blue-400 text-white'
           : 'bg-gray-100 text-gray-700 hover:bg-gray-200',
